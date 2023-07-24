b094c5334cb199453bd5c10a49394b26f86777a6
Evan Sicoli <evan.sicoli@stonybrook.edu>
2023-07-24 14:03:14 -0400

    fourth commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+const {readFile} = require('fs').promises
+
+const start = async () => {
+    const p1 = await readFile('./test1.txt', 'utf8')
+    const p2 = await readFile('./test2.txt', 'utf8')
+
+    console.log(p1)
+    console.log(p2)
+}
+
+start()
